b0ec8dd665679efe11e77de133db55a4f782f7a6
Dane Meide <dsmeide@gmail.com>
2024-03-06 21:30:16 -0600

    created botscore for localStorage

--- local-storage-botScore-button/js/main.js
+++ local-storage-botScore-button/js/main.js
@@ -1,3 +1,14 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
+if(!localStorage.getItem('botScore')) {
+    localStorage.setItem('botScore', 0);
+}
 
 
+document.querySelector('button').addEventListener('click', addToBotScore);
+
+function addToBotScore() {
+    let botScore = Number(localStorage.getItem('botScore'));
+    botScore = botScore + 1;
+    localStorage.setItem('botScore', botScore);
+
+}
